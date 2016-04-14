c1bffd922968cf54376dec61da42b98038a9dbd8
nicktheway <nikospa96@hotmail.gr>
2016-04-15 00:02:54 +0300

    Αρχείο header
    Με τα προτυπα των συναρτήσεων και τη δομή μιγαδικών

--- A2-8551/operations.h
+++ A2-8551/operations.h
@@ -2,7 +2,7 @@ struct complex
 {
     float re;
     float im;
-};
+}; 
 float add(float num1, float num2);
 complex add(float re1, float re2, float im1, float im2);
 float sub(float num1, float num2);
